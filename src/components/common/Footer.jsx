417b913434f39ec8fda38702746f963d84ac48a2
BearK <harry.hsu0515@gmail.com>
2023-01-11 14:48:34 +0800

    import footer logo

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import { FacebookIcon, LineIcon } from "../../assets/icons";
+import { FacebookIcon, HomeIcon, LineIcon, PowerByIcon } from "../../assets/icons";
+import smallLogo from "../../assets/icons/logo-white.png";
 
 const FooterStyled = styled.footer`
   display: flex;
@@ -16,20 +17,35 @@ const FooterStyled = styled.footer`
 
     .left {
       display: flex;
-      *{
+      align-items: center;
+      * {
         margin: 0 10px;
+      }
+      .brand-logo {
+        width: 100px;
+        height: 100px;
+        img {
+          width: 100%;
+        }
       }
     }
 
     .social-network {
       display: flex;
-      * {
-        margin: 0 10px;
-      }
-
+      gap: 10px;
       .icon {
         width: 30px;
         height: 30px;
+      }
+      .homepage {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 30px;
+        height: 30px;
+        border-radius: 50%;
+        background-color: var(--gray-dark);
+        line-height: 30px;
       }
     }
   }
@@ -49,38 +65,42 @@ const FooterStyled = styled.footer`
 export default function Footer() {
   return (
     <FooterStyled>
-      <div className="footer infos">
-        <div className="left">
-          <div className="brand-logo">logo</div>
-          <div className="info-title">
+      <div className='footer infos'>
+        <div className='left'>
+          <div className='brand-logo'>
+            <img src={smallLogo} alt='' />
+          </div>
+          <div className='info-title'>
             關於
-            <div className="info">聯絡我們</div>
-            <div className="info">部落格</div>
+            <div className='info'>聯絡我們</div>
+            <div className='info'>部落格</div>
           </div>
-          <div className="info-title">
+          <div className='info-title'>
             全部商品
-            <div className="info">訂單查詢</div>
-            <div className="info">訂單相關說明</div>
+            <div className='info'>訂單查詢</div>
+            <div className='info'>訂單相關說明</div>
           </div>
-          <div className="info-title">
+          <div className='info-title'>
             付款方式說明
-            <div className="info">寄送方式說明</div>
-            <div className="info">售後服務說明</div>
+            <div className='info'>寄送方式說明</div>
+            <div className='info'>售後服務說明</div>
           </div>
-          <div className="info-title">
+          <div className='info-title'>
             會員權益說明
-            <div className="info">現金積點規則</div>
-            <div className="info">隱私權條款</div>
+            <div className='info'>現金積點規則</div>
+            <div className='info'>隱私權條款</div>
           </div>
         </div>
-        <div className="social-network">
-          <FacebookIcon className="icon" />
-          <LineIcon className="icon" />
-          <div className="icon">h</div>
+        <div className='social-network'>
+          <FacebookIcon className='icon' />
+          <LineIcon className='icon' />
+          <div className='icon homepage'>
+            <HomeIcon />
+          </div>
         </div>
       </div>
-      <div className="footer down">
-        <div className="customer-service">
+      <div className='footer down'>
+        <div className='customer-service'>
           <div>客服專線:</div>
           <div>02-3322-5628</div>
           <div>地址:</div>
@@ -89,7 +109,7 @@ export default function Footer() {
           <div>週一 ~ 週五</div>
           <div>09:00~12:30 ; 13:30~18:00</div>
         </div>
-        <div className="power-by">powered by W A C A</div>
+        <PowerByIcon />
       </div>
     </FooterStyled>
   );
