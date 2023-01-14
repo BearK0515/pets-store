b7cfaa5633cff3cb2d05dd3391ba5feb6a5b37e6
kignite <b82732433@hotmail.com.tw>
2023-01-14 16:49:23 +0800

    search-bar冒泡事件未解決

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -131,7 +131,7 @@ const Layout = () => {
     <>
       <StyledContainer
         onClick={() => {
-          console.log(searchBarRef.current.className)
+          setSearchBarActive(false)
         }}
       >
         <Header />
