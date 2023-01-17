ccd72d744210f56a56382dbc592b968fd6a79071
chuchushooes <chuchushooes@gmail.com>
2023-01-17 09:53:33 +0800

    gotop排版

--- src/components/common/GoTop.jsx
+++ src/components/common/GoTop.jsx
@@ -3,9 +3,13 @@ import styled from "styled-components";
 import { BiArrowToTop } from "react-icons/bi";
 import ScrollToTop from "react-scroll-to-top";
 
-
 const GoTopStyled = styled.ul`
+  .top-button {
+  }
   .top-icon {
+    display: flex;
+    justify-content: center;
+    align-items: center;
     height: 50px;
     width: 50px;
     border-width: 1px;
@@ -14,23 +18,31 @@ const GoTopStyled = styled.ul`
     position: fixed;
     right: 20px;
     bottom: 20px;
-    opacity: .8;
+    opacity: 0.8;
     background-color: var(--gray-dark);
     font-size: 3em;
     animation: move 0.5s ease-in infinite alternate;
   }
-   @keyframes move {
-      from {transform: translate(0,0)};
-      to {transform: translate(0,2px)};
+  @keyframes move {
+    from {
+      transform: translate(0, 0);
+    }
+    to {
+      transform: translate(0, 2px);
+    }
   }
 `;
 
-export default function GoTop () {
+export default function GoTop() {
   return (
     <GoTopStyled>
-      <button>
-        <ScrollToTop smooth className="top-icon" component={<BiArrowToTop style={{ color: "white" }}/>} />
+      <button className='top-button'>
+        <ScrollToTop
+          smooth
+          className='top-icon'
+          component={<BiArrowToTop style={{ color: "white" }} />}
+        />
       </button>
     </GoTopStyled>
   );
-};
+}
