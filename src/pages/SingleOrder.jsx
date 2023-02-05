d51ee4124bca3cd36a9e12dd379ea0366bbf01cc
BearK <harry.hsu0515@gmail.com>
2023-02-05 17:18:12 +0800

    刪除無用程式碼

--- src/pages/SingleOrder.jsx
+++ src/pages/SingleOrder.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import { singleOrder } from "../api/adminAuth";
 
